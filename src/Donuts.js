2ad82695560030159855555c3846409b9e2bed76
kimsese694@gmail.com <@ki>
2023-12-19 07:43:47 +0800

    Added Donuts.js file

---
+++ src/Donuts.js
@@ -0,0 +1,22 @@
+export const donutsList = [
+    {
+		id: "01",
+		name: "Baked S'mores Donuts",
+		image: require("../assets/donutslist/Baked S'mores Donuts.jpg"),
+		rating: "4.2",
+		ingredients: ["Fresh Tuna", "Lime Juice", "Red Onion", "Avocado"],
+		time: "₱ 28",
+		difficulty: "Medium",
+		calories: "420 cal",
+		color: "#006A4E",
+		description:
+			"A delightful and refreshing dish featuring fresh tuna mixed with zesty lime juice and the crunch of red onion and avocado.",
+		steps: [
+			"Dice the fresh tuna into small cubes.",
+			"Finely chop the red onion and avocado.",
+			"Mix the tuna, red onion, and avocado in a bowl.",
+			"Drizzle lime juice over the mixture and gently toss.",
+			"Serve chilled and enjoy!",
+		],
+	},
+]
